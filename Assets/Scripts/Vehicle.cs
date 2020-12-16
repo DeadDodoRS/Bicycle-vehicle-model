32c462d6d355dc3be21a404f29930b5fe22fbc5b
Sergey Ivanov <imports@mail.ru>
2020-12-16 16:34:42 +0300

    Init commit

---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    [SerializeField] private float _angle;
+    [SerializeField] private float _angleLimit = 25;
+
+    [SerializeField] private float _wheelForce = 2;
+
+    [SerializeField] private float _mass = 1000;
+
+    [Header("Visual")]
+    [SerializeField] private GameObject _frontWheel;
+
+    public float al = 1;
+    public float bl = 1;
+
+    public float inertia = 1;
+
+    public float fx1 = 0;
+    public float fx2 = 0;
+    public float fy1 = 0;
+    public float fy2 = 0;
+
+
+    [Header("Debug")]
+    public float CurrentSpeedX = 0;
+    public float CurrentSpeedY = 0;
+
+    public float CurrentAcccelerationX = 0;
+    public float CurrentAcccelerationY = 0;
+
+    public float time = 0;
+
+    // TODO: Посмотреть код чела!
+
+    private void LateUpdate()
+    {
+        _angle = Mathf.Clamp(_angle, -_angleLimit, _angleLimit);
+        _frontWheel.transform.localEulerAngles = new Vector3(0, 0, -_angle);
+
+        // This is local acceleration
+        CurrentAcccelerationX = (fx1 * Mathf.Cos(_angle) + fx2 - fy1 * Mathf.Sin(_angle)) / _mass;
+        CurrentAcccelerationY = (fx1 * Mathf.Sin(_angle) + fy1 * Mathf.Cos(_angle) + fy2) / _mass;
+
+        CurrentSpeedX = CurrentSpeedX + CurrentAcccelerationX * Time.deltaTime;
+        CurrentSpeedY = CurrentSpeedY + CurrentAcccelerationY * Time.deltaTime;
+
+        float offsetX = CurrentSpeedX * Time.deltaTime + CurrentAcccelerationX * Mathf.Pow(Time.deltaTime, 2) / 2;
+        float offsetY = CurrentSpeedY * Time.deltaTime + CurrentAcccelerationY * Mathf.Pow(Time.deltaTime, 2) / 2;
+
+        // Convert acceleration from car space to global cords
+        float carRotation = -transform.eulerAngles.z;
+
+        float a = Mathf.Sin((carRotation * Mathf.PI) / 180);
+        float b = Mathf.Cos((carRotation * Mathf.PI) / 180);
+
+        float worldOffsetX = offsetX * a + offsetY * a;
+        float worldOffsetY = offsetX * b + offsetY * b;
+
+        float newPosX = transform.position.x + worldOffsetX;
+        float newPosY = transform.position.y + worldOffsetY;
+
+        transform.position = new Vector3(newPosX, newPosY, 0);
+
+        float inertia = 1;
+
+        float addRotationAngle = (al * fx1 * Mathf.Sin(_angle) + al * fy1 * Mathf.Cos(_angle) - bl * fy2) / 2;
+        addRotationAngle *= Time.deltaTime;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + addRotationAngle);
+    }
+}
---
+++ Assets/Scripts/VehicleSimple.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleSimple : MonoBehaviour
+{
+    [SerializeField] private GameObject _frontWheel;
+
+    [SerializeField] private float _length = 1;
+
+    [SerializeField] private float lr = 0.5f;
+
+    [SerializeField] private float _acceleration;
+    [SerializeField] private float _velocity;
+    [SerializeField] private float _angle;
+    [SerializeField] private float _angleLimit = 30;
+
+    [Header("Keyboard control")]
+    public float accelSpeed = 2;
+    public float turningSpeed = 2;
+
+    private void LateUpdate()
+    {
+        UpdateInput();
+        WheelUpdate();
+
+        // Calculate slip angle
+        float radiansAngle = _angle * Mathf.Deg2Rad;
+        float slipAngle = Mathf.Atan(lr * Mathf.Tan(radiansAngle) / _length);
+
+        // Calculate position
+        float carRotation = transform.eulerAngles.z;
+
+        //_velocity = _velocity + _acceleration * Time.deltaTime;
+
+        float velX = _velocity * Mathf.Cos((carRotation + slipAngle) * Mathf.Deg2Rad);
+        float velY = _velocity * Mathf.Sin((carRotation + slipAngle) * Mathf.Deg2Rad);
+
+        float worldOffsetX = velX * Time.deltaTime;
+        float worldOffsetY = velY * Time.deltaTime;
+
+        float newPosX = transform.position.x + worldOffsetX;
+        float newPosY = transform.position.y + worldOffsetY;
+        transform.position = new Vector3(newPosX, newPosY, 0);
+
+
+        // Calculate orientation
+        float s = _length / Mathf.Tan(radiansAngle);
+        float turningRadius = s / Mathf.Cos(slipAngle * Mathf.Deg2Rad);
+        float angularVelocity = _velocity / turningRadius;
+
+        float addRotationAngle = angularVelocity * Time.deltaTime * Mathf.Rad2Deg;
+
+        Debug.LogError($"radius: {turningRadius}; vel: {_velocity}; angVel:{angularVelocity}");
+
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + addRotationAngle);
+    }
+
+    private void WheelUpdate()
+    {
+        _angle = Mathf.Clamp(_angle, -_angleLimit, _angleLimit);
+        _frontWheel.transform.localEulerAngles = new Vector3(0, 0, _angle);
+    }
+
+    private void UpdateInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            _acceleration += accelSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            _acceleration -= accelSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _angle += turningSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _angle -= turningSpeed * Time.deltaTime;
+        }
+    }
+}
