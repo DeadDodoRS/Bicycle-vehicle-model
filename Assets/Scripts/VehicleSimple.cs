2781670f2d5a4561dcd7d330bd7069908c470672
Sergey Ivanov <imports@mail.ru>
2020-12-20 13:35:11 +0300

    Upd

--- Assets/Scripts/VehicleSimple.cs
+++ Assets/Scripts/VehicleSimple.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class VehicleSimple : MonoBehaviour
 {
+    [SerializeField] private GameObject _camera;
+    [SerializeField] private float _offset;
+
     [SerializeField] private GameObject _frontWheel;
 
-    [SerializeField] private float _length = 1;
-
-    [SerializeField] private float lr = 0.5f;
-
-    [SerializeField] private float _acceleration;
-    //[SerializeField] private float _velocity;
     [SerializeField] private float _angle;
     [SerializeField] private float _angleLimit = 30;
 
     [Header("Keyboard control")]
+    public bool KeyboardEnable = false;
+    public bool IsCameraFollow = false;
+
     public float accelSpeed = 2;
     public float turningSpeed = 2;
 
     [Header("Test")]
-    private float x_dot = 1;
+    [SerializeField] private float a = 1f;
+    [SerializeField] private float b = 1f;
+
+    [SerializeField] private float x_dot = 1;
     private float x_ddot = 0;
 
     private float y_dot;
@@ -35,12 +38,13 @@ public class VehicleSimple : MonoBehaviour
 
     [SerializeField] private float inertia = 1;
 
+    [Header("PD-gains")]
+    [SerializeField] private float Kp = 2;
+    [SerializeField] private float Kd = 4;
+
 
     private void Physics()
     {
-        float a = 1f;
-        float b = 1f;
-
         y_dot = y_dot + y_ddot * Time.deltaTime;
         thetta_dot = thetta_dot + thetta_ddot * Time.deltaTime;
 
@@ -51,15 +55,13 @@ public class VehicleSimple : MonoBehaviour
 
         y_ddot = - x_dot * thetta_dot + 1 / mass * (c1 * Mathf.Cos(delta) * (delta - frontSlip) - c2 * backSlip);
         thetta_ddot = 1 / inertia * (a * c1 * Mathf.Cos(delta) * (delta - frontSlip) + b * c2 * backSlip);
-
-        Debug.Log($"{y_ddot}; {thetta_ddot}");
     }
 
     private void UpdateVehicle()
     {
         // Position
         float carRotation = transform.eulerAngles.z;
-        x_dot = x_dot + _acceleration * Time.deltaTime;
+        x_dot = x_dot + x_ddot * Time.deltaTime;
 
         float velX = x_dot * Mathf.Cos(carRotation * Mathf.Deg2Rad) + y_dot * Mathf.Sin(carRotation * Mathf.Deg2Rad);
         float velY = x_dot * Mathf.Sin(carRotation * Mathf.Deg2Rad) + y_dot * Mathf.Cos(carRotation * Mathf.Deg2Rad);
@@ -79,44 +81,35 @@ public class VehicleSimple : MonoBehaviour
 
     private void LateUpdate()
     {
+        if (!KeyboardEnable)
+            UpdateControl();
+        else
+            UpdateInput();
+        
         WheelUpdate();
-        UpdateInput();
 
         Physics();
         UpdateVehicle();
 
-        //WheelUpdate();
+        if(IsCameraFollow)
+            _camera.transform.position = new Vector3(transform.position.x + _offset, _camera.transform.position.y, _camera.transform.position.z);
+    }
 
-        //// Calculate slip angle
-        //float radiansAngle = _angle * Mathf.Deg2Rad;
-        //float slipAngle = Mathf.Atan(lr * Mathf.Tan(radiansAngle) / _length);
+    private float deltaPosError = 0;
+    private float prevY;
 
-        //// Calculate position
-        //float carRotation = transform.eulerAngles.z;
+    private void UpdateControl() 
+    {
+        deltaPosError = (prevY - transform.position.y) * 1 / Time.deltaTime;
+        prevY = transform.position.y;
 
-        //x_dot = x_dot + _acceleration * Time.deltaTime;
-
-        //float velX = x_dot * Mathf.Cos((carRotation + slipAngle) * Mathf.Deg2Rad);
-        //float velY = x_dot * Mathf.Sin((carRotation + slipAngle) * Mathf.Deg2Rad);
-
-        //float worldOffsetX = velX * Time.deltaTime;
-        //float worldOffsetY = velY * Time.deltaTime;
-
-        //float newPosX = transform.position.x + worldOffsetX;
-        //float newPosY = transform.position.y + worldOffsetY;
-        //transform.position = new Vector3(newPosX, newPosY, 0);
+        float direction = -Mathf.Sign(transform.position.y);
+        float posError = Mathf.Abs(transform.position.y);
 
 
-        //// Calculate orientation
-        //float s = _length / Mathf.Tan(radiansAngle);
-        //float turningRadius = s / Mathf.Cos(slipAngle * Mathf.Deg2Rad);
-        //float angularVelocity = x_dot / turningRadius;
+        _angle = _angleLimit * (direction * posError * Kp + deltaPosError * Kd);
 
-        //float addRotationAngle = angularVelocity * Time.deltaTime * Mathf.Rad2Deg;
-
-        //Debug.LogError($"radius: {turningRadius}; vel: {x_dot}; angVel:{angularVelocity}");
-
-        //transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + addRotationAngle);
+        Debug.LogError($"P: {direction * posError * Kp}; D: {deltaPosError * Kd}; Angle: {_angle}");
     }
 
     private void WheelUpdate()
@@ -127,16 +120,6 @@ public class VehicleSimple : MonoBehaviour
 
     private void UpdateInput()
     {
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    _acceleration += accelSpeed * Time.deltaTime;
-        //}
-
-        //if (Input.GetKey(KeyCode.S))
-        //{
-        //    _acceleration -= accelSpeed * Time.deltaTime;
-        //}
-
         if (Input.GetKey(KeyCode.A))
         {
             _angle += turningSpeed * Time.deltaTime;
